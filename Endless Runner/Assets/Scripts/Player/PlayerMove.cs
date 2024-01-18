d8222cfb8395f741cc370595bc95204a71a35132
Jakub Poreda <j.poreda02@gmail.com>
2024-01-18 20:10:09 +0100

    Dodanie logiki + animacji skoku

--- Endless Runner/Assets/Scripts/Player/PlayerMove.cs
+++ Endless Runner/Assets/Scripts/Player/PlayerMove.cs
@@ -8,6 +8,11 @@ public class PlayerMove : MonoBehaviour
     public float moveSpeed = 30;
     public float leftRightSpeed = 4;
     static public bool canMove = false;
+    public bool isJumping = false;
+    public bool goingDown = false;
+    public GameObject playerObject;
+    private float jumpStrength = 8;
+
     private void Start()
     {
         
@@ -33,6 +38,45 @@ public class PlayerMove : MonoBehaviour
                     transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
                 }
             }
+
+
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Space))
+            {
+               if(isJumping == false)
+                {
+                    isJumping = true;
+                    playerObject.GetComponent<Animator>().Play("Jump");
+                    StartCoroutine(JumpSequence());
+                }
+            }
+        }
+        if(isJumping)
+        {
+            if(goingDown == false)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * jumpStrength, Space.World);
+            }
+            
+
+            if (goingDown == true)
+            {
+                transform.Translate(Vector3.down * Time.deltaTime * jumpStrength, Space.World);
+            }
+        }
+    }
+
+    IEnumerator JumpSequence()
+    {
+        float initialHeight = transform.position.y;
+        yield return new WaitForSeconds(0.33f);
+        goingDown = true;
+        yield return new WaitForSeconds(0.33f);
+        isJumping = false;
+        goingDown = false;
+        if (ObstacleCollision.isCollision == false)
+        {
+            transform.position = new Vector3(transform.position.x, initialHeight, transform.position.z);
+            playerObject.GetComponent<Animator>().Play("Standard Run");
         }
     }
 }
